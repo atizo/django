fcbf2610ed5f5180c2d827583e1a2834b5191f6b
Marco Schmalz <marco@atizo.com>
2012-11-22 21:29:32 +0100

    Allow aggregation results to be string values

--- django/db/backends/__init__.py
+++ django/db/backends/__init__.py
@@ -737,7 +737,7 @@ class BaseDatabaseOperations(object):
             return value
         elif internal_type and internal_type.endswith('IntegerField') or internal_type == 'AutoField':
             return int(value)
-        elif internal_type in ('DateField', 'DateTimeField', 'TimeField'):
+        elif internal_type in ('DateField', 'DateTimeField', 'TimeField', 'CharField', 'TextField'):
             return value
         # No field, or the field isn't known to be a decimal or integer
         # Default to a float
